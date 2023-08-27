104bc1555ef523ab2b9eb917a06d613a8ae2e768
LyokoJeremie <lucheng989898@gmail.com>
2023-08-27 11:47:46 +0800

    DelayInfo

---
+++ DelayInfo.ts
@@ -0,0 +1,29 @@
+/**
+ * Socks5BalancerAsio : A Simple TCP Socket Balancer for balance Multi Socks5 Proxy Backend Powered by Boost.Asio
+ * Copyright (C) <2020>  <Jeremie>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import moment from 'moment';
+import 'moment/locale/zh-cn.js';
+import Vue from 'vue';
+import _ from "lodash";
+
+import 'i18n-table/en-US';
+import 'i18n-table/zh-CN';
+import {I18NTableType, zhCN} from "./i18n-table/zh-CN";
+import {enUS} from "./i18n-table/en-US";
+
+console.log('1')
